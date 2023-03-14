518e645f62c9a2c0809e663bac7191326cdd06c8
gilvancarvalho <gilvancarvalho@sefaz.pi.gov.br>
2023-03-14 13:24:44 -0300

    implementação do filtro de token

---
+++ src/main/java/br/org/sindojus/core/SecurityFilter.java
@@ -0,0 +1,21 @@
+package br.org.sindojus.core;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+
+    }
+}
